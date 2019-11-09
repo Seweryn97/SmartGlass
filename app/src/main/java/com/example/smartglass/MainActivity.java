647bdc74c84d4d1d857f2e2554d262c3ee71839b
Seweryn <sewerynde@gmail.com>
2019-11-09 01:48:14 +0100

    reading from accelerometr

--- app/src/main/java/com/example/smartglass/MainActivity.java
+++ app/src/main/java/com/example/smartglass/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.smartglass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private static final String TAG= "MainActivity";
+
+    private SensorManager sensorManager;
+    Sensor accelerometr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometr = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometr, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registered accelerometer listener");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent ) {
+        if(sensorEvent.values[0]>0 && sensorEvent.values[2]>0) Log.d(TAG,"Góra, Prawo");
+        else if (sensorEvent.values[0]>0 && sensorEvent.values[2]<0) Log.d(TAG,"Góra, Lewo");
+        else if (sensorEvent.values[0]<0 && sensorEvent.values[2]>0) Log.d(TAG,"Dół, Prawo");
+        else Log.d(TAG,"Dół,Lewo");
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
