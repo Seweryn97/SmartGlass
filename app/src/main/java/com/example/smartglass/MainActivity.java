474e7be0807eadc342b0eaf4d49bf0eec9f60e08
Seweryn <sewerynde@gmail.com>
2019-11-14 23:41:19 +0100

    not working

--- app/src/main/java/com/example/smartglass/MainActivity.java
+++ app/src/main/java/com/example/smartglass/MainActivity.java
@@ -10,37 +10,69 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private static final String TAG= "MainActivity";
+    private static final String TAG = "MainActivity";
 
     private SensorManager sensorManager;
-    Sensor accelerometr;
+    Sensor accelerometer;
+    Sensor gyroscope;
+    TextView xvalue, yvalue, gyrovalue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate: Initializing Sensor Services");
+        xvalue = (TextView) findViewById(R.id.xvalue);
+        yvalue = (TextView) findViewById(R.id.yvalue);
+        gyrovalue = (TextView) findViewById(R.id.gyrovalue);
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        accelerometr = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(MainActivity.this, accelerometr, SensorManager.SENSOR_DELAY_NORMAL);
-        Log.d(TAG, "onCreate: Registered accelerometer listener");
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        if (gyroscope != null) {
+            sensorManager.registerListener(MainActivity.this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+        } else xvalue.setText("Not connected");
+
+        //if (accelerometer != null) {
+            sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+       // } else xvalue.setText("Not connected");
+
+
+
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent ) {
-        if(sensorEvent.values[0]>0 && sensorEvent.values[2]>0) Log.d(TAG,"Góra, Prawo");
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xvalue.setText("X: " + sensorEvent.values[0]);
+            yvalue.setText("Y: " + sensorEvent.values[1]);
+            Log.d(TAG,"X :"+sensorEvent.values[0]);
+            Log.d(TAG," Y :"+sensorEvent.values[1]);
+
+        }
+        /*if(sensorEvent.values[0]>0 && sensorEvent.values[2]>0) Log.d(TAG,"Góra, Prawo");
         else if (sensorEvent.values[0]>0 && sensorEvent.values[2]<0) Log.d(TAG,"Góra, Lewo");
         else if (sensorEvent.values[0]<0 && sensorEvent.values[2]>0) Log.d(TAG,"Dół, Prawo");
-        else Log.d(TAG,"Dół,Lewo");
+        else Log.d(TAG,"Dół,Lewo")*/
 
-    }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        else if(sensor.getType()==Sensor.TYPE_GYROSCOPE)
+        {
+            gyrovalue.setText("Gyro: " + sensorEvent.values[1]);
+            Log.d(TAG," Gyro :"+sensorEvent.values[1]);
+        }
     }
 }
+
